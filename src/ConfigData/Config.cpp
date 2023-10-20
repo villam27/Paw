6f832dad0cd456e11d4c845474e4ada637474aa5
villam27 <villam2703@protonmail.com>
2023-10-20 16:02:24 +0200

    [Add] Config.cpp and doc

--- src/ConfigData/Config.cpp
+++ src/ConfigData/Config.cpp
@@ -1,70 +1,52 @@
-#include "LuaC.h"
+#include <Config.h>
 #include <iostream>
 
-LuaC::LuaC() {
-    _L = luaL_newstate();
-    if (_L == NULL) {
-        throw new std::runtime_error("LuaC: Could not create Lua state.");
-    }
+Config::Config() {
+    if (!_lua.LoadScript("res/config.lua"))
+        throw new std::runtime_error("Config: Cannot load res/config.lua");
 }
 
-LuaC::~LuaC() {
-    lua_close(_L);
+Config::Config(const std::string &path) {
+    if (!_lua.LoadScript(path.c_str()))
+        throw new std::runtime_error(std::string("Config: Cannot load " + path));
 }
 
-bool LuaC::LoadScript(const std::string &path) {
-    int r = luaL_loadfile(_L, path.c_str());
-    return CheckLua(r);
+Config::~Config() {
+
 }
 
-bool LuaC::LoadScriptFromMemory(const std::string &script) {
-    int r = luaL_loadstring(_L, script.c_str());
-    return CheckLua(r);
+bool Config::LoadAll() {
+    if (LoadAppData() == false)
+		return (false);
+	if (LoadSheets() == false)
+		return (false);
+	if (LoadAnimation() == false)
+		return (false);
+	return (true);
 }
 
-bool LuaC::CheckLua(int r) {
-    if (r != LUA_OK) {
-        std::cerr << "LuaC: " << lua_tostring(_L, -1) << std::endl;
-        return false;
-    }
-    return true;
+bool Config::LoadAppData() {
 }
 
-bool LuaC::CallFunction(const std::string &functionName, int numArgs, int numResults) {
-    lua_getglobal(_L, functionName.c_str());
-    DumpStack();
-    if (!lua_isfunction(_L, -1))
-        return false;
-    if (!CheckLua(lua_pcall(_L, numArgs, numResults, 0)))
-        return false;
-    return true;
+bool Config::LoadSheets() {
+    return (true);
 }
 
-void LuaC::DumpStack() {
-    int i;
-    int top = lua_gettop(_L);
+bool Config::LoadAnimation() {
+    return (true);
+}
 
-    std::cout << "\n>>	LUA	STACK	<<\n" << std::endl;
-    for (i = 1; i <= top; i++) {
-        int t = lua_type(_L, i);
-        switch (t) {
-            case LUA_TSTRING:
-                std::cout << lua_tostring(_L, i) << std::endl;
-                break;
-            case LUA_TBOOLEAN:
-                std::cout << (lua_toboolean(_L, i) ? "true" : "false") << std::endl;
-                break;
-            case LUA_TNUMBER:
-                std::cout << lua_tonumber(_L, i) << std::endl;
-                break;
-            case LUA_TNIL:
-                std::cout << "nil";
-                break;
-            default:
-                std::cout << lua_typename(_L, t) << std::endl;
-                break;
-        }
-        std::cout << " ";
-    }
-    std::cout << "\n>>	END	STACK	<<\n" << std::endl;
+const AppData *Config::GetAppData(void)
+{
+	return (&_appData);
+}
+
+const std::vector<Sheet> *Config::GetSheets(void)
+{
+	return (&_sheets);
+}
+
+const std::unordered_map<std::string, Animation> *Config::GetAnimation(void)
+{
+	return (&_animations);
 }
--- src/ConfigData/include/Config.h
+++ src/ConfigData/include/Config.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <LuaC.h>
 #include <vector>
+#include <unordered_map>
 
 /**
  * @defgroup DataStructures Data Structures
@@ -71,6 +72,105 @@ struct Animation
  */
 class Config {
 private:
+	typedef	std::vector<Sheet>::const_iterator c_sheet_it;
+	typedef	std::vector<Rect>::const_iterator c_sprite_it;
+	typedef	std::unordered_map<std::string, Animation>::const_iterator c_anim_it;
+	typedef	std::vector<int>::const_iterator c_frame_it;
+
+private:
+    LuaC _lua;
+    AppData _appData;
+	std::vector<Sheet> _sheets;
+	std::unordered_map<std::string, Animation> _animations;
+	int _r;
+
+public:
+    /**
+     * @brief Initializes a Config object from res/config.lua.
+     */
+	Config(void);
+
+    /**
+     * @brief Initializes a Config object from a specified file.
+     * @param path The path to the configuration file.
+    */
+	Config(const std::string &path);
+
+    /**
+     * @brief Destroys a Config object.
+    */
+	~Config(void);
+
+    /**
+     * @brief Loads all configuration data.
+     * @return True if all data was loaded successfully, false otherwise.
+    */
+	bool LoadAll(void);
+
+    /**
+     * @brief Loads application data.
+     * @return True if the data was loaded successfully, false otherwise.
+    */
+	bool LoadAppData(void);
+
+    /**
+     * @brief Loads sprite sheet data.
+     * @return True if the data was loaded successfully, false otherwise.
+    */
+	bool LoadSheets(void);
+
+    /**
+     * @brief Loads animation data.
+     * @return True if the data was loaded successfully, false otherwise.
+    */
+	bool LoadAnimation(void);
+
+    /**
+     * @brief Get the application data.
+     * @return AppData.
+    */
+	const AppData *GetAppData(void);
+
+    /**
+     * @brief Get the sprite sheet data.
+     * @return A vector of Sheet objects.
+    */
+	const std::vector<Sheet> *GetSheets(void);
+
+    /**
+     * @brief Get the animation data.
+     * @return A unordered map of Animation objects.
+    */
+	const std::unordered_map<std::string, Animation> *GetAnimation(void);
+
+    /**
+     * @brief Get a pointer to an animation.
+     * @param name The name of the animation.
+    */
+	const Animation *GetAnimationPtr(const std::string& name);
+
+	//lua_State						*GetState(void) { return (_L); }
+	//int							GetReturn(void) { return (_r); }
+
+    /**
+     * @brief Print All Data.
+    */
+	void DumpAll(void);
+
+    /**
+     * @brief Print Application Data.
+    */
+	void DumpAppData(void);
+
+    /**
+     * @brief Print Sprite Sheet Data.
+    */
+    void DumpSheets(void);
+
+    /**
+     * @brief Print Animation Data.
+    */
+	void DumpAnimation(void);
 };
 
 #endif // CONFIG_H
