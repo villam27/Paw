880e323d1999d489032b876d1c2f3276dd5dfd10
villam27 <villam2703@protonmail.com>
2023-10-20 14:13:37 +0200

    [Test] LuaClass

--- src/LuaClass/LuaC.cpp
+++ src/LuaClass/LuaC.cpp
@@ -30,29 +30,41 @@ bool LuaC::CheckLua(int r) {
     return true;
 }
 
+bool LuaC::CallFunction(const std::string &functionName, int numArgs, int numResults) {
+    lua_getglobal(_L, functionName.c_str());
+    DumpStack();
+    if (!lua_isfunction(_L, -1))
+        return false;
+    if (!CheckLua(lua_pcall(_L, numArgs, numResults, 0)))
+        return false;
+    return true;
+}
+
 void LuaC::DumpStack() {
     int i;
     int top = lua_gettop(_L);
+
+    std::cout << "\n>>	LUA	STACK	<<\n" << std::endl;
     for (i = 1; i <= top; i++) {
         int t = lua_type(_L, i);
         switch (t) {
             case LUA_TSTRING:
-                std::cout << lua_tostring(_L, i);
+                std::cout << lua_tostring(_L, i) << std::endl;
                 break;
             case LUA_TBOOLEAN:
-                std::cout << (lua_toboolean(_L, i) ? "true" : "false");
+                std::cout << (lua_toboolean(_L, i) ? "true" : "false") << std::endl;
                 break;
             case LUA_TNUMBER:
-                std::cout << lua_tonumber(_L, i);
+                std::cout << lua_tonumber(_L, i) << std::endl;
                 break;
             case LUA_TNIL:
                 std::cout << "nil";
                 break;
             default:
-                std::cout << lua_typename(_L, t);
+                std::cout << lua_typename(_L, t) << std::endl;
                 break;
         }
         std::cout << " ";
     }
-    std::cout << std::endl;
+    std::cout << "\n>>	END	STACK	<<\n" << std::endl;
 }
--- src/LuaClass/include/LuaC.h
+++ src/LuaClass/include/LuaC.h
@@ -11,7 +11,7 @@ extern "C" {
 
 /**
  * @class LuaC
- * @brief Represents a C++ interface for working with Lua scripting.
+ * @brief A C++ interface for working with Lua scripting.
  */
 class LuaC {
 private:
@@ -20,6 +20,7 @@ private:
 public:
     /**
      * @brief Initializes a LuaC object.
+     * @throw std::runtime_error if the Lua state could not be created.
      */
     LuaC();
 
@@ -56,6 +57,15 @@ public:
      * @return True if r == LUA_OK, false otherwise.
     */
     bool CheckLua(int r);
+
+    /**
+     * brief Calls a Lua function.
+     * @param functionName The name of the function.
+     * @param nArgs The number of arguments.
+     * @param nResults The number of results.
+     * @return True if the function was called successfully, false otherwise.
+    */
+    bool CallFunction(const std::string &functionName, int nArgs, int nResults);
 
     /**
      * @brief Print the Lua stack.
